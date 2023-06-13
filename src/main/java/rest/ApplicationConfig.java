2672336b21a62541f37cc5ba41decb4ad876b83e
Berry1196 <Sebastianberry11@gmail.com>
2023-06-13 09:23:23 +0200

    Made entities, DTOs, and deleted resource

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -26,7 +26,7 @@ public class ApplicationConfig extends Application {
         resources.add(security.RolesAllowedFilter.class);
         resources.add(security.errorhandling.AuthenticationExceptionMapper.class);
         resources.add(security.errorhandling.NotAuthorizedExceptionMapper.class);
-        resources.add(rest.TripResource.class);
+
 
     }
     
